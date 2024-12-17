21eb505244c83cf5a96eab095ce2218270812164
Manu <Manu@Manu786>
2024-12-17 00:37:11 -0500

    swap of 2 numbers done with or without third var

--- src/javaassignment16Dec2024/SwapTwoNumbers.java
+++ src/javaassignment16Dec2024/SwapTwoNumbers.java
@@ -4,15 +4,16 @@ public class SwapTwoNumbers {
 
 	int a, b;
 
-	SwapTwoNumbers(int a, int b) {
+	SwapTwoNumbers(int a, int b) {  //parameterised constructor to initiate var a, b 
 		this.a = a;
 		this.b = b;
 		System.out.println("value of a before swapping: " + a); //a=10
 		System.out.println("value of b before swapping: " + b); //b=20
 	}
 
+	// using third var
 	void swapUsingThirdVar() {
-		// using third var
+		
 		int c;
 
 		c = a;  // c=10
@@ -21,11 +22,13 @@ public class SwapTwoNumbers {
 
 		System.out.println("value of a after swapping using third var: " + a);  //a=20
 		System.out.println("value of b after swapping using third var: " + b);  //b=10
+		
 	}
 
+	// without using third var 
 	void swapWithoutThirdVar() {
 
-		// without using third var using operator+
+		//using operator+
 		a = a + b; // a=10+20= 30
 		b = a - b; // b=30-20= 10
 		a = a - b; // c=30-10= 20
@@ -42,7 +45,7 @@ public class SwapTwoNumbers {
 
 	public static void main(String[] args) {
 
-		new SwapTwoNumbers(10, 20).swapUsingThirdVar();
+		new SwapTwoNumbers(10, 20).swapUsingThirdVar();  // creating object of class using its parameterised constructor and initiating var a, b 
 		new SwapTwoNumbers(10, 20).swapWithoutThirdVar();
 
 	}
