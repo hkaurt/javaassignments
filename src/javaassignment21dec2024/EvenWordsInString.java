6e8a416d8f206cc835122ed16d2373e6448420be
Manu <Manu@Manu786>
2024-12-22 22:42:25 -0500

    assignment 21 dec 2024 EvenWordsInString done

--- src/javaassignment21dec2024/EvenWordsInString.java
+++ src/javaassignment21dec2024/EvenWordsInString.java
@@ -4,39 +4,39 @@ public class EvenWordsInString {
 
 	public static void main(String[] args) {
 
-		String str = "Print even word from this sentence ";
-		
-		int evenWordCount = 0; // count number of even words
+		String sentence = "Print words which have even number of letters from this sentence ";
+
+		int evenWordCount = 0; // count number of words with even number of letters
 
 		// edge cases:
 		/*
-		 * 1. If string is not blank and even words present then print 
-		 * 2. No even word found 
+		 * 1. If string is not blank and words with even number of letters found then print those words
+		 * 2. No word with even number of letters found 
 		 * 3. String is blank
 		 */
-		
+
 		// condition if string is not blank
-		if (!str.isBlank()) {
+		if (!sentence.isBlank()) {
 
 			// spilt string into array of words
-			String[] strA = str.split(" ");
+			String[] words = sentence.split(" ");
 
 			// read each word ele of String array
-			for (String st : strA) {
-
-				// condition if length of each word is even count then print the word
-				if (st.length() % 2 == 0) {
-					System.out.println(st);
-					evenWordCount++; // increase evenWordCount each time even word is found
+			for (String word : words) {
+				
+				// condition if length of each word is even then print the word
+				if (word.length() % 2 == 0) {
+					System.out.println(word);
+					evenWordCount++; // increase evenWordCount each time if length of word is found even
 				}
 			}
 
-			// condition if string not blank but no even word
+			// condition if string not blank but no word with even number of letters found
 			if (evenWordCount == 0) {
-				System.out.println("no even word found, the sentence contains all odd words");
+				System.out.println("no word with even number of letters found, the length of each word is odd in the given sentence");
 			}
 
-		} else                        //if String is blank
+		} else // String is blank
 			System.out.println("String is blank");
 	}
 }
